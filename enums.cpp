080787cfa4c52a726f3b0e3761b2fbda08d78681
w1c1y1 <akulov.max3003@gmail.com>
2026-02-15 12:30:28 +0700

    enums

--- enums.cpp
+++ enums.cpp
@@ -1,3 +1,4 @@
+
 enum alu3_ind_op_type{
     BIT,
     CMP = 6
@@ -21,4 +22,57 @@ enum mem3_opt_type{
 
 enum imm9_op_type{
     INT = 0, RESET = 1, PUSH = 2, ADDSP = 6, JSR = 8 
+};
+
+enum zero_op {
+    ZERO, RESERVED1,
+    RESERVED2, RESERVED3,
+    HALT, WAIT,
+    EI, DI,
+    JSR, PUPC, 
+    POPC, PUSP,
+    POSP, PUPS,
+    POPS,
+};
+
+
+enum one_op {
+    PUSH, POP, 
+    LDI, JSRR, 
+    LDSP, STSP, 
+    LDPC, STPS, 
+    LDPC, STPC, 
+    ADDSP
+};
+
+enum two_op {
+    MOVE
+};
+
+enum mem2_op {
+    LDW, LBD, 
+    LDSB, LEW, 
+    LEB, LCSB, 
+    STW, STB
+};
+
+enum imm_6_op {
+    LSW, LSB, 
+    LSSB, SSW, 
+    SSB, LDI, 
+    ADD, CMP,
+};
+
+enum GROUP {
+    ZERO_OP = 0b0, 
+    ONE_OP = 0b001, 
+    TWO_OP = 0b01000, 
+    MEM_2 = 0b01010, 
+    IMM_6 = 0b011,
+    IMM_9 = 0b100, 
+    MEM_3 = 0b1010, 
+    BR_ABS = 0b00001, 
+    BR_REL_N = 0b110, 
+    BR_REL_P = 0b111,
+    SHIFTS = 0b0001
 };
