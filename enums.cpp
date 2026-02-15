143bc6ccbca4c5df319cdac9c895acaf6f87aa9f
EugeneKornev <yevgene.kornev@gmail.com>
2026-02-15 08:29:12 +0300

    Enums2

--- enums.cpp
+++ enums.cpp
@@ -1,8 +1,24 @@
 enum alu3_ind_op_type{
-    bit,
-    cmp = 6
+    BIT,
+    CMP = 6
 };
 
 enum alu3_op_type{
-    and_, or_, xor_, bic_, add_, addc_,  sub_, subc_
+    AND, OR, XOR, BIC, ADD, ADDC,  SUB, SUBC
+};
+
+enum alu2_op_type{
+    NEG, NOT, SXT, SCL
+};
+
+enum shifts_op_type{
+    SHL, SHR, SHRA, ROL, ROR, RCL, RCR
+};
+
+enum mem3_opt_type{
+    LDW, LDB, LDSB, LCW, LCB, LCSB, STW, STB
+};
+
+enum imm9_op_type{
+    INT = 0, RESET = 1, PUSH = 2, ADDSP = 6, JSR = 8 
 };
